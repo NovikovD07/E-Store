b7fac5096aed3b53182b35b02b047e9116f5c029
NovikovD07 <daniilan1993@gmail.com>
2023-12-10 01:06:11 +0300

    CRUD, filtration, pagination

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express');
 const router = new Router();
+const DeviceController = require('../controllers/deviceController');
+const deviceController = require('../controllers/deviceController');
 
-router.post('/');
-router.get('/');
-router.get('/:id');
+router.post('/', deviceController.create);
+router.get('/', deviceController.getAll);
+router.get('/:id', deviceController.getOne);
 // router.delete('/:brandId')
 
 module.exports = router;
